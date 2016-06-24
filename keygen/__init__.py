d99006ddf3c444285d1a408bbfc3587aeae1a03e
al3xv3gas <officialalexvegas@gmail.com>
2016-06-24 10:17:49 +0000

    Update __init__.py

--- keygen/__init__.py
+++ keygen/__init__.py
@@ -1,3 +1,7 @@
+# AL3X V3GAS KeyGen
+# AL3X V3GAS 2016 (C)
+# The AVX Project 2016 (C)
+
 import random
 
 def keygen():
@@ -13,3 +17,15 @@ def keygen():
     let2 = random.choice(rp2)
     codef = str(let1) + str(code1) + str(let2) + str(code2)
     return codef
+
+def small():
+    rp1 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    rp2 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    rp3 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+    rp4 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+    let1 = random.choice(rp1)
+    let2 = random.choice(rp2)
+    let3 = random.choice(rp3)
+    let4 = random.choice(rp4)
+    code = str(rp1) + str(rp3) + str(rp4) + str(rp2)
+    return code
